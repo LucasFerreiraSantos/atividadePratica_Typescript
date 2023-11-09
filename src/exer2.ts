c564c7f12c47035461426bd10a5f0bc1b8dccc2b
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-11-08 21:14:28 -0300

    feito o exer2

--- src/exer2.ts
+++ src/exer2.ts
@@ -1,4 +1,39 @@
+type gradesWeights = [
+    {
+        nota: number;
+        peso: number
+    },
+    {
+        nota: number;
+        peso: number
+    },
+    {
+        nota: number;
+        peso: number
+    }
+]
 
+const list: gradesWeights = [
+    {
+        nota: 10,
+        peso: 3
+    },
+    {
+        nota: 8,
+        peso: 5
+    },
+    {
+        nota: 9,
+        peso: 2
+    }
+]
+
+function averageWeights(list: gradesWeights){
+    const finalGrade: number = (list[0].nota * list[0].peso) + (list[1].nota * list[1].peso) + (list[2].nota * list[2].peso) / 3
+    return finalGrade
+}
+
+console.log(`A média final baseada no peso é: `,averageWeights(list))
 
 // 2. Crie uma função que receba uma lista de objetos contendo nota e
 // peso, realize a média das notas considerando o peso. Exemplos:
