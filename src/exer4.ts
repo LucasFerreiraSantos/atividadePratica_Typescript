834f5c8bbcb4c4c4d9ee89314bf1325d86f997a6
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-11-10 22:34:50 -0300

    feito o exer4

--- src/exer4.ts
+++ src/exer4.ts
@@ -1,4 +1,25 @@
+let listFruits: string[] = [];
 
+function putFruit(fruit: string): void{
+    listFruits.push(fruit)
+}
+
+function listingFruits(){
+    console.log(listFruits)
+}
+
+function deleteFruits(list: string[], stringRemove: string): string[] {
+    return list.filter(fruit => fruit === stringRemove);
+}
+
+putFruit("Banana")
+putFruit("Manga")
+putFruit("Morango")
+
+const fruitsRemoves = deleteFruits(listFruits, "Banana");
+console.log(fruitsRemoves)
+
+console.log(listingFruits())
 
 // 4. Crie um programa para cadastrar, listar e excluir produtos de uma
 // lista com tipagem de Produto.
