b1e463a2b119e53367bb7058b0ac59ed3984f601
root <root@LUCAS>
2023-11-12 22:15:33 -0300

    feito o exer

--- src/exer5.ts
+++ src/exer5.ts
@@ -1,27 +1,34 @@
 export type User = {
-    name: string;
-    age: number;
-    occupation: string;
-    wage?: number | string;
+  name: string;
+  age: number;
+  occupation: string;
+  wage?: number | string;
+};
+
+function createUser(User: {
+  name: string;
+  age: number;
+  occupation: string;
+  wage?: number | string;
+}): User {
+  if (User.wage === undefined) {
+    User.wage = "N/A";
+  }
+
+  const employee: User = {
+    name: User.name,
+    age: User.age,
+    occupation: User.occupation,
+    wage: User.wage,
+  };
+
+  return employee;
 }
 
-function createUser(User: { name: string, age: number, occupation: string, wage?: number | string }): User {
-    if(User.wage === undefined) {
-        User.wage = "N/A"
-    }
-
-    const employee: User = {
-        name: User.name,
-        age: User.age,
-        occupation: User.occupation,
-        wage: User.wage
-    }
-
-    return employee
-}
-
-const user = createUser({name: "Lucas", age: 25, occupation: "Logística" })
-console.log(`O Funcionário ${user.name} tem ${user.age} anos, trabalha com ${user.occupation} e possui um salário de ${user.wage}`)
+const user = createUser({ name: "Lucas", age: 25, occupation: "Logística" });
+console.log(
+  `O Funcionário ${user.name} tem ${user.age} anos, trabalha com ${user.occupation} e possui um salário de ${user.wage}`
+);
 
 // 5. Crie um programa para mostrar informações de usuários (User) de
 // uma empresa. Crie o tipo User com as seguintes propriedades:
